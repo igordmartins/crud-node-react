8b6e7296b4e6b8d21940684ed739c689bdcf676e
igor martins <igordavillamartins@outlook.com>
2022-08-01 19:30:03 -0300

    50% done

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "718293ig",
+  database: "crudgames",
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let SQL = "INSERT INTO games ( name, cost, category ) VALUES ( ?,?,? )";
+
+  db.query(SQL, [name, cost, category], (err, res) => {
+    console.log(err)
+  })
+})
+
+app.get("/getCards", (req, res) => {
+
+    let SQL = "SELECT * FROM games"
+
+    db.query(SQL, (err, result) => {
+      if(err) console.log(err);
+      else res.send(result);
+    })
+})
+
+app.listen(3001, () => {
+  console.log("rodando servidor");
+})
